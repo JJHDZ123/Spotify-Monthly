20b182a55409461aeadd2effaeffe4c55715e1ca
Juan H <juantwothreeinseries@gmail.com>
2022-09-13 21:56:42 -0500

    deployment 3 bug fix

--- utils/Constants.js
+++ utils/Constants.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect_uri = 'http://localhost:5000/auth/callback';
+const redirect_uri = 'https://spotify-monthly-1.herokuapp.com//auth/callback';
 
 module.exports = { CLIENT_ID, CLIENT_SECRET, redirect_uri };
